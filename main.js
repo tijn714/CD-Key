f47d8acf8a2c703dddfb95cb08426a75f34d70ef
Tijn Rodrigo <71492011+tijn714@users.noreply.github.com>
2023-10-05 17:52:17 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,102 @@
+const fs = require('fs');
+const crypto = require('crypto');
+
+var generatedKeys = [];
+var hashedKeys = [];
+
+let filename = generateFileName();
+
+
+
+let logo =  [
+"     ____ ____        _  __          ",
+"    / ___|  _ \\      | |/ /___ _   _ ",
+"   | |   | | | |_____| ' // _ \\ | | |",
+"   | |___| |_| |_____| . \\  __/ |_| |",
+"    \\____|____/      |_|\\_\\___|\\__, |",
+"                               |___/ ",
+"  By: Tijn Rodrigo                                 " 
+]                                              
+
+// check if the output folder exists, if not, create it
+if (!fs.existsSync("output")) {
+    fs.mkdirSync("output");
+}
+
+
+console.log(logo.join("\n"));
+
+function generateKey() {
+    var key = "";
+    var sectors = 5; // Modify this to change the number of sectors, 5 is the default
+    var sectorLength = 5; // Modify this to change the length of each sector (in characters), 5 is the default
+    var possible = "ACDEFGHIJKLMNOPQRSTUVXY01245789";
+
+    var usedCharacters = {};
+
+    for (var i = 0; i < sectors; i++) {
+        var sector = "";
+
+        for (var j = 0; j < sectorLength; j++) {
+            var char;
+            do {
+                char = possible.charAt(Math.floor(Math.random() * possible.length));
+            } while (usedCharacters[char] >= 2);
+
+            sector += char;
+            usedCharacters[char] = (usedCharacters[char] || 0) + 1;
+        }
+
+        key += sector;
+        if (i < sectors - 1) key += "-";
+    }
+
+    generatedKeys.push(key);
+
+    const sha256 = crypto.createHash('sha256');
+    sha256.update(key);
+    const hashedKey = sha256.digest('hex');
+    hashedKeys.push(hashedKey);
+
+    return key;
+}
+
+var numKeysToGenerate = 10;
+for (var i = 0; i < numKeysToGenerate; i++) {
+    generateKey();
+}
+
+const data = {
+    generatedKeys: generatedKeys,
+    hashedKeys: hashedKeys
+};
+
+
+// function to generate the .json filename (date + hashed md5 date).json
+function generateFileName() {
+    var date = new Date();
+    var dateStr = date.toISOString().replace(/:/g, "-");
+    var md5 = crypto.createHash('md5');
+    md5.update(dateStr);
+    var md5Str = md5.digest('hex');
+    return "."+dateStr + "-" + md5Str + ".json";
+}
+
+fs.writeFile("output/"+filename, JSON.stringify(data, null, 2), (err) => {
+    if (err) {
+        console.log(" Your CD keys have been generated successfully.\n");
+        console.log(" Generated keys:");
+        console.log(generatedKeys);
+        console.log(" Hashed keys:");
+        console.log(hashedKeys);
+        console.error(` Something when wrong while saving your CD keys to 'output/${filename}'`);
+        console.error(err);
+    } else {
+        console.log(" Your CD keys have been generated successfully.\n");
+        console.log(" Generated keys:");
+        console.log(generatedKeys);
+        console.log(" Hashed keys:");
+        console.log(hashedKeys);
+        console.log(` Your CD keys have been saved successfully to 'output/${filename}'`);
+    }
+});
---
+++ readme.md
@@ -0,0 +1,84 @@
+# CD Key Generator
+
+This Node.js application generates CD keys for your software or product. The generated CD keys are displayed and saved in a JSON file.
+
+<img src="img/Screenshot 2023-10-05 at 17.29.25.png" draggable="false">
+
+## Prerequisites
+
+Before using this program, make sure you have the following installed:
+
+- [Node.js](https://nodejs.org/) - JavaScript runtime environment
+
+## Getting Started
+
+1. Clone or download the repository to your local machine.
+
+2. Navigate to the project directory using your terminal or command prompt.
+
+3. Install the required dependencies by running the following command:
+
+   ```shell
+   npm install
+   ```
+
+4. To start the CD key generation, run the following command:
+
+   ```shell
+   npm start
+   ```
+
+## Sample Output
+
+After running the program, you will see an output similar to the following:
+
+```
+     ____ ____        _  __
+    / ___|  _ \      | |/ /___ _   _
+   | |   | | | |_____| ' // _ \ | | |
+   | |___| |_| |_____| . \  __/ |_| |
+    \____|____/      |_|\_\___\__, |
+                               |___/
+  By: Tijn Rodrigo
+ Your CD keys have been generated successfully.
+
+ Generated keys:
+[
+  '5EUQ2-5LQ7G-YHRDE-AANUF-H7CC9',
+  '202K1-EEMN9-SQK0H-AGTHA-ISDMP',
+  'FQJX9-7QOSO-AYNGF-95Y25-IIAN0',
+  '57LNK-27THN-494UM-YLK1I-F2X8M',
+  'KKNTD-XQDXG-FASY2-HCQVC-LT8S4',
+  'N1VNE-9TS20-XLXY4-80A59-Y715Q',
+  'S1FEA-NVTEQ-5US5T-JM8F9-A04HG',
+  'CIDHR-4MHG7-U7ON2-E4GCE-8VRIX',
+  'MRSEC-U7UNJ-ORSJN-C4EHI-MK879',
+  '4H97Q-LJYVO-IYD8P-7LR8V-HEJK1'
+]
+
+ Hashed keys:
+[
+  '4140168e20ec217329f564ce7e497d9c9c7e909390873b060320be84f9f0efd8',
+  'c63cd73d5afa08ee7e171119ed9081bbc57ef1ebcbd7bf052248acdf6ec274f8',
+  'e8cbdd626d6a944d3819ecf10dae135b78acb7c3c866bc77780879acda36c1a8',
+  '0961954b05d42a3753a9f7c9341367d95d15ae54b905e9c59b912a2bac7e38e1',
+  '290c39adc659e63b0a463bc9efb275b464723cff4681e1ecc1fc16da1bdc7510',
+  '64eadaeb2db2ea8420c7cf7c3be315fe51572b7c5e283731adafd8ca3920e01a',
+  'bd1acf472e3a8d60cbddb3516ab5e86a5a7a2fa7929d49d84c4a51dcb61df372',
+  '77ae4d87afa7f2d13c6578039c1114ce641201439cd7097c2a248a7b78dd6c23',
+  'd3f0793e663f8f4b03a55b64ebcbedf2898992c53def6157c63ac34c487b20e3',
+  '02031a0803853f4d8023acd81e8c6efa595978a7e90974fdf340687bed89920e'
+]
+
+ Your CD keys have been saved successfully to 'output/.2023-10-05T15-29-22.031Z-cb854fb01c206eca336fc5ff4b0adf2a.json'
+```
+
+<b style="color:red;">*WARNING*: THE GENERATED JSON FILES ARE HIDDEN BECAUSE OF THE `.` IN FRONT OF THE .JSON FILE.</b>
+
+## Author
+
+- Tijn Rodrigo
+
+## License
+
+This program is open-source and available under the [MIT License](LICENSE). Feel free to use, modify, and distribute it as needed.
